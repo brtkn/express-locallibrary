64e65a6d7eecffcafda89d88c5efc6c2df204ad5
brtkn <newcreativedesingofficial@gmail.com>
2021-04-20 00:15:10 +0300

    added mongodb url

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var express = require('express');
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var dev_db_url = 'mongodb+srv://bodin:b1e3r0a1t@cluster0.fimbu.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://bodin:c2er4aa@cluster0.fimbu.mongodb.net/local_library?retryWrites=true&w=majority';
 var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
